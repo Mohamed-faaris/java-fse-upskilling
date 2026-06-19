53b1168f902c68c56ec1a3476b15ecbd6108f58c
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-06-20 05:26:06 +0530

    react lab

--- week-01/engineering-concepts/algorithms-data-structures/exercise-7-mandatory/Main.java
+++ week-01/engineering-concepts/algorithms-data-structures/exercise-7-mandatory/Main.java
@@ -30,3 +30,8 @@ public class Main {
         System.out.println(predictFutureValue(10));
     }
 }
+
+
+
+
+
---
+++ week-07/react/react-lab/lab0/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <h1>Welcome to React Lab!</h1>
+  <p>This is a simple React application to get you started with React development.</p>
+    </>
+  )
+}
+
+export default App
