e3266c16953d8c27cd6785d9ace67f207b554df1
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-07-02 15:08:11 +0530

    jpa

--- java-labs/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
+++ java-labs/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -1,13 +1,33 @@
 package com.cognizant.ormlearn;
 
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class OrmLearnApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+
     public static void main(String[] args) {
-        SpringApplication.run(OrmLearnApplication.class, args);
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        logger.info("Inside main");
+        countryService = context.getBean(CountryService.class);
+        testGetAllCountries();
     }
 
+    private static void testGetAllCountries() {
+        logger.info("Start");
+        List<Country> countries = countryService.getAllCountries();
+        logger.info("Countries : {}", countries);
+        logger.info("End");
+    }
 }
